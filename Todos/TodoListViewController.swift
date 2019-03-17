62908cb5eeeb934e23b5835f2fb106e1f6a73521
John C. Kennedy <devjckennedy@gmail.com>
2019-03-17 12:16:24 +0900

    Todo list complete with datasource and delegate methods

---
+++ Todos/TodoListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TodoListViewController.swift
+//  Todos
+//
+//  Created by John C. Kennedy on 2019/03/17.
+//  Copyright © 2019 John C. Kennedy. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK - Tabelview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK - Tabelview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        // Give selected row a checkmark accessory
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        // Remove highlight after selecting a row
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
