f9e47585684d067433172b6fd079bfb7080f9a9f
John C. Kennedy <devjckennedy@gmail.com>
2019-03-17 10:46:35 +0900

    Initial Commit

---
+++ Todos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todos
+//
+//  Created by John C. Kennedy on 2019/03/17.
+//  Copyright © 2019 John C. Kennedy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
